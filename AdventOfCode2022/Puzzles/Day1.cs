cd64c4bf461ebd52ff143f667f6e6714aa08b8a6
Diamond3 <diamond322222@gmail.com>
2024-10-30 22:28:45 +0200

    Add Day1 solution

--- AdventOfCode2022/Puzzles/Day1.cs
+++ AdventOfCode2022/Puzzles/Day1.cs
@@ -1,11 +1,79 @@
 ﻿using AdventOfCode2022.Utils;
+using System.Runtime.CompilerServices;
 
 namespace AdventOfCode2022.Puzzles;
 
 public class Day1 : ISolver
 {
+    private string fileName = "Inputs/Day1.txt";
+    private int max = 0;
+    private int trioMin = 0;
+
+    private (int x, int y, int z) topTrio;
+
     public void Solve()
     {
-        throw new NotImplementedException();
+        try
+        {
+            using var sr = new StreamReader(fileName);
+
+            var goNext = false;
+            var tempSum = 0;
+
+            while (sr.ReadLine() is string line)
+            {
+                if (line.Length == 0)
+                {
+                    if (trioMin < tempSum)
+                    {
+                        topTrio = AddToTopTrio(tempSum);
+                        trioMin = topTrio.x;
+                    }
+
+                    tempSum = 0;
+                }
+                else
+                {
+                    tempSum += int.Parse(line);
+                }
+            }
+
+            if (trioMin < tempSum)
+            {
+                topTrio = AddToTopTrio(tempSum);
+                trioMin = topTrio.x;
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"An error occurred: {e.Message}");
+        }
+
+        Console.WriteLine(topTrio.x + topTrio.y + topTrio.z);
+    }
+
+    private (int, int, int) AddToTopTrio(int currentVal)
+    {
+        var x = topTrio.x;
+        var y = topTrio.y;
+        var z = topTrio.z;
+
+        if (currentVal > topTrio.z)
+        {
+            x = y;
+            y = z;
+            z = currentVal;
+        }
+        else if (currentVal > topTrio.y)
+        {
+            x = y;
+            y = currentVal;
+        }
+        else if (currentVal > topTrio.x)
+        {
+            x = currentVal;
+        }
+
+        return (x, y, z);
     }
 }
