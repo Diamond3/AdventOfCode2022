a746a42ab60ba9964f6bc375c93b5e1f20a8b1ff
Diamond3 <diamond322222@gmail.com>
2024-11-06 23:14:54 +0200

    Add Day6-1

---
+++ AdventOfCode2022/Puzzles/Day6.cs
@@ -0,0 +1,37 @@
+﻿using AdventOfCode2022.Utils;
+
+namespace AdventOfCode2022.Puzzles;
+
+public class Day6 : ISolver
+{
+    private string fileName = $"Inputs/{nameof(Day6)}.txt";
+
+    public string Solve()
+    {
+        try
+        {
+            using var sr = new StreamReader(fileName);
+            var str = sr.ReadLine()!;
+            var hashSet = new HashSet<char>();
+
+            for (int i = 3; i < str.Length; i++)
+            {
+                hashSet.Clear();
+                for (int j = i - 3; j <= i; j++)
+                {
+                    hashSet.Add(str[j]);
+                }
+                if (hashSet.Count == 4)
+                {
+                    return (i + 1).ToString();
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"An error occurred: {e.Message}");
+        }
+
+        return "";
+    }
+}
